b0f32ff9ebeef87803b7b59ec5d05e721fc73205
Paul Schuetz <pa.schuetz@web.de>
2022-03-16 20:47:50 +0100

    Update the library used for the cli, deepen the functions
    The cli now uses the new library, not the deprecated one, and gets the
    functionality not only to run the assembly-code, but also to execute single
    steps or instructions, and can print deeper information about the cpu and
    memory.
    
    Signed-off-by: Paul Schuetz <pa.schuetz@web.de>

--- CPU-Simulation-CLI/Utilities.swift
+++ CPU-Simulation-CLI/Utilities.swift
@@ -7,55 +7,125 @@
 
 import Foundation
 import CPU_Simulation_Lib
+import CPU_Simulation_Utilities
 
-func getMemoryRepresentation(mem: Memory, results: ParseResults) -> String {
-    var result = "Memory:\n"
-    var usedLines: [UInt16] = []
-    var lastLineUsed = true
+func getBusses(cpu: CPU, assemblingResults: AssemblingResults) -> String {
+    var addressString = "Empty"
+    var dataString = "Empty"
     
-    for i in 0 ... "FFFF".uHex {
-        let val = i & 0xFFF0
-        if mem.read(address: i) != 0 && !usedLines.contains(val) {
-            usedLines.append(val)
-        }
+    if let addressBus = cpu.addressBus, let dataBus = cpu.dataBus {
+        addressString = toLongHexString(addressBus)
+        
+        let valueType = assemblingResults.memoryValues[addressBus] ?? AssemblingResults.AddressAddressValue()
+        
+        dataString = valueType.transformOnlyNumber(value: dataBus)
     }
+    
+    var result =    "Address-Bus:\t\(addressString)\n"
+    result +=       "Data-Bus:   \t\(dataString)\n\n"
+    
+    
+    return result
+}
+
+func getMemoryRepresentation(cpu: CPU, mem: Memory, results: AssemblingResults) -> String {
+    var result = "Memory:\n"
+    
+    result += getBusses(cpu: cpu, assemblingResults: results)
+    
+    let usedLines = calculateUsedLines(memory: mem)
     
     if usedLines.isEmpty {
         result += "No Values"
         return result
     }
     
-    var table: [[String]] = [["","0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]]
-    
-    for i in 0 ... "FFF".uHex {
-        let nr = i << 4
-        if usedLines.contains(nr) {
-            var addition = [hex(nr, long: false)]
-            for j in 0 ... "F".uHex {
-                let address = nr | j
-                let val = mem.read(address: address)
-                let output: String
-                if let op = results.operators[address] {
-                    output = op.string
-                } else if results.addresses.contains(address) {
-                    output = "0x" + hex(val)
-                } else {
-                    output = String(val)
-                }
-                addition.append(output)
-            }
-            table.append(addition)
-            lastLineUsed = true
-        } else {
-            if lastLineUsed {
-                let addition: [String] = Array(repeating: "...", count: 17)
-                table.append(addition)
-                lastLineUsed = false
-            }
-        }
-    }
+    let table = createTable(usedLines, assemblingResults: results, memory: mem)
     
     result += getTable(values: table, header: true, additionalDistance: 2, unifiedDistance: true)
     
     return result
 }
+
+
+private func getStartOfTable() -> [[String]] {
+    return [["","0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]]
+}
+
+private func calculateUsedLines(memory: Memory) -> [UInt16] {
+    var usedLines: [UInt16] = []
+    
+    for i in 0 ... UInt16(0xffff) {
+        let val = i & 0xFFF0
+        if memory.read(address: i) != 0 && !usedLines.contains(val) {
+            usedLines.append(val)
+        }
+    }
+    
+    return usedLines
+}
+
+private func createFirstAddressFromLineNr(_ i: UInt16) -> UInt16 {
+    return i << 4
+}
+
+private func testIfLineWasUsed(usedLines: [UInt16], lineNr: UInt16) -> Bool {
+    return usedLines.contains(lineNr)
+}
+
+private func createStartOfNewLine(_ firstAddressNr: UInt16) -> [String] {
+    return [toHexString(firstAddressNr)]
+}
+
+private func calculateAddressFromLineAndNr(lineNr: UInt16, address: UInt16) -> UInt16 {
+    return lineNr | address
+}
+
+private func getStringOfAddress(memory: Memory, lineNr: UInt16, address: UInt16, assemblingResults: AssemblingResults) -> String {
+    let realAddress = calculateAddressFromLineAndNr(lineNr: lineNr, address: address)
+    let value = memory.read(address: realAddress)
+    
+    let valueType = assemblingResults.memoryValues[realAddress] ?? AssemblingResults.AddressAddressValue()
+    
+    return valueType.transform(value: value)
+}
+fileprivate func addAddressToLine(line: inout [String], lineNr: UInt16, address: UInt16, assemblingResults: AssemblingResults, memory: Memory) {
+    line.append(getStringOfAddress(memory: memory, lineNr: lineNr, address: address, assemblingResults: assemblingResults))
+}
+
+fileprivate func handleUsageOfLastLineInResultString(lastLineUsed:  Bool, result: inout [[String]]) {
+    if lastLineUsed {
+        let addition: [String] = Array(repeating: "...", count: 17)
+        result.append(addition)
+    }
+}
+
+fileprivate func appendUsedLine(firstAddressNr: UInt16, result: inout [[String]], assemblingResults: AssemblingResults, memory: Memory) {
+    var newLine = createStartOfNewLine(firstAddressNr)
+    
+    for j in 0 ... UInt16(0xf) {
+        addAddressToLine(line: &newLine, lineNr: firstAddressNr, address: j, assemblingResults: assemblingResults, memory: memory)
+    }
+    
+    result.append(newLine)
+}
+
+private func createTable(_ usedLines: [UInt16], assemblingResults: AssemblingResults, memory: Memory) -> [[String]] {
+    var result = getStartOfTable()
+    var lastLineUsed = true
+    
+    for i in 0 ... UInt16(0xfff) {
+        let firstAddressNr = createFirstAddressFromLineNr(i)
+        
+        if testIfLineWasUsed(usedLines: usedLines, lineNr: firstAddressNr) {
+            appendUsedLine(firstAddressNr: firstAddressNr, result: &result, assemblingResults: assemblingResults, memory: memory)
+            lastLineUsed = true
+        } else {
+            handleUsageOfLastLineInResultString(lastLineUsed: lastLineUsed, result: &result)
+            lastLineUsed = false
+        }
+    }
+    
+    return result
+}
+
--- CPU-Simulation-CLI/main.swift
+++ CPU-Simulation-CLI/main.swift
@@ -7,30 +7,38 @@
 
 import Foundation
 import CPU_Simulation_Lib
+import CPU_Simulation_Utilities
+import CloudKit
+
+var executionCycles = UInt(0)
 
 try main()
 
 func main() throws {
     var args = CommandLine.arguments
     
-    let s = parseCLIArgs(args: &args)
+    let parsedArgs = parseCLIArgs(args: &args)
+    
+    let s = parsedArgs.startingPoint ?? 0
     
     if args.count < 2 {
-        print("No file is specified!")
-        print("Specify the location of a assembler-file!")
-        print("You can also specify the starting point in memory with the '-s'-flag and a decimal\nor hexadecimal number, which is positive and smaller than 65536 or 0x10000.")
+        print("No file is specified!\n")
+        print("Specify the location of a assembler-file!\n")
+        print("You can also specify the starting point in memory with the '-s'-flag and a decimal\nor hexadecimal number, which is positive and smaller than 65536 or 0x10000.\n")
+        print("If the cpu should directly execute the code until the first HOLD-Instruction, use the '-r'-flag")
         return
     }
     
     let fm = FileManager.default
     
     if args[1] == "-h" {
-        print("Specify the location of a assembler-file!")
-        print("You can also specify the starting point in memory with the '-s'-flag and a decimal\nor hexadecimal number, which is positive and smaller than 65536 or 0x10000.")
+        print("Specify the location of a assembler-file!\n")
+        print("You can also specify the starting point in memory with the '-s'-flag and a decimal\nor hexadecimal number, which is positive and smaller than 65536 or 0x10000.\n")
+        print("If the cpu should directly execute the code until the first HOLD-Instruction, use the '-r'-flag")
         return
     } else if args[1] == "-v" {
-        let version = "1.0.1"
-        let date = "12.1.2022"
+        let version = "2.0"
+        let date = "16.3.2022"
         print("CPU-Simulation-CLI, Paul Schütz/Paul Schuetz\nVersion \(version)\n\(date)")
         return
     }
@@ -50,137 +58,288 @@ func main() throws {
     
     let mem = Memory()
     
-    let cpu = CPU(programStart: s,memory: mem)
+    let cpu = CPU(memory: mem, startingPoint: s)
     
-    var parseResults: ParseResults? = nil
     do {
-        parseResults = try parseAssembler(input: assembler, mem: mem)
-        
-        
-        var continueRunning = true
-        
-        while continueRunning {
-            try cpu.run()
-            
-            continueRunning = printAll(cpu: cpu, mem: mem, parseResults: parseResults!, continueRunningAllowed: true)
-        }
-    } catch ParsingError.operatorStringDoesNotExist(let input, let lineNr) {
-        print("The operator \"\(input)\" in line \(lineNr) couldn't be parsed!")
-    } catch ParsingError.literalIsNotAllowed(let input, let lineNr) {
-        print("The operator \"\(input)\" in line \(lineNr) couldn't be parsed, because it's not allowed to receive a literal!")
-    } catch ParsingError.doubleLiteral(let input, let lineNr) {
-        print("The operator \"\(input)\" in line \(lineNr) tries to declare a literal twice, which isn't allowed!")
-    } catch ParsingError.hasToBeLiteral(let input, let lineNr) {
-        print("The operator \"\(input)\" in line \(lineNr) has to receive an simple literal, everything else, even literals in relation of the stackpointer aren't allowed.")
-    } catch ParsingError.wrongNumberOfArguments(let input, let mode, let lineNr) {
-        if mode {
-            print("The operator \"\(input)\" in line \(lineNr) has to receive a operator, but does not!")
+        let assemblingResult = try mem.loadAssembly(assemblyCode: assembler)
+
+
+        if parsedArgs.instantRun {
+            try operateWithFirstRun(cpu: cpu, assemblingResults: assemblingResult)
         } else {
-            print("The operator \"\(input)\" in line \(lineNr) is not allowed to receive a operator, but does so!")
+            try operate(cpu: cpu, assemblingResults: assemblingResult)
         }
-    } catch ParsingError.unreadableAddressInput(let input, let lineNr) {
-        print("The address or literal \"\(input)\" in line \(lineNr) couldn't be parsed!")
-    } catch ExecutionError.decodingError(let opcode, let address) {
-        print("Error: the opcode \(hex(opcode)) (\(opcode)) at address 0x\(hex(address)) cannot be decoded.")
-        _ = printAll(cpu: cpu, mem: mem, parseResults: parseResults!, continueRunningAllowed: false)
-    } catch ExecutionError.cannotReceiveLiteral(let opcode, let operatorString, let address) {
-        print("Error: the operator \"\(operatorString)\" with the opcode \(hex(opcode)) (\(opcode)) at address 0x\(hex(address)) cannot receive a literal, but does so.")
-        _ = printAll(cpu: cpu, mem: mem, parseResults: parseResults!, continueRunningAllowed: false)
-    } catch ExecutionError.mustReceiveLiteral(let opcode, let operatorString, let address) {
-        print("Error: the operator \"\(operatorString)\" with the opcode \(hex(opcode)) (\(opcode)) at address 0x\(hex(address)) has to receive a literal, but doesn't.")
-        _ = printAll(cpu: cpu, mem: mem, parseResults: parseResults!, continueRunningAllowed: false)
     } catch {
         print("Error: \(error)")
     }
 }
 
-func getCPUString(cpu: CPU) -> String {
-    return "CPU:\nprogram counter: 0x\(hex(cpu.programCounterExternal))\t\tstack-pointer: 0x\(hex(cpu.stackpointerExternal))\naccumulator: \(cpu.accumulatorExternal)\t\t\texecution cycles: \(cpu.cycleCountExternal)\n"
+func printSeparation() {
+    print("\n\n\n\n\n")
 }
 
-func getVars(parseResult: ParseResults, mem: Memory) -> String {
-    var result = "Variables:\n"
-    let vars = parseResult.varList
-    
-    for variable in vars {
-        if parseResult.operators[variable.1] != nil {
-            continue
-        }
-        result += "\"\(variable.0)\" at 0x\(hex(variable.1)): "
-        if parseResult.addresses.contains(variable.1) {
-            result += "0x" + hex(mem.read(address: variable.1))
-        } else {
-            result += String(mem.read(address: variable.1))
-        }
-        result += "\n"
+func printSuccess() {
+    print("Execution Successfull\n")
+}
+
+func increaseCycleCountIfNecessary(cpu: CPU) {
+    if cpu.state == "executed" || cpu.state == "hold" {
+        executionCycles &+= 1
     }
+}
+
+func operate(cpu: CPU, assemblingResults results: AssemblingResults) throws {
+    var operationMode = OperationMode.stop
+    let memory = cpu.memory
     
-    result += "\nJump-Markers:\n"
-    
-    for variable in vars {
-        if parseResult.operators[variable.1] == nil {
-            continue
+    repeat {
+        
+        printAll(cpu: cpu, mem: memory, assemblingResults: results)
+        
+        operationMode = getOperationMode(cpu: cpu)
+        
+        switch operationMode {
+            case .nextStep:
+                try cpu.operateNextStep()
+                
+                increaseCycleCountIfNecessary(cpu: cpu)
+                
+                printSeparation()
+                printSuccess()
+            case .endInstruction:
+                try cpu.endInstruction()
+                executionCycles &+= 1
+                
+                printSeparation()
+                printSuccess()
+            case .run:
+                try cpu.run()
+                executionCycles &+= cpu.cycleCount
+                
+                printSeparation()
+                printSuccess()
+            case .stop:
+                break
         }
-        result += "\"\(variable.0)\" at 0x\(hex(variable.1)): "
-        result += "\(parseResult.operators[variable.1]!.string) (0x\(hex(mem.read(address: variable.1))))"
-        result += "\n"
-    }
+        
+    } while operationMode != .stop
+}
+
+func operateWithFirstRun(cpu: CPU, assemblingResults results: AssemblingResults) throws {
+    print("Instant Running")
+    
+    try cpu.run()
+    
+    printSuccess()
+    
+    executionCycles &+= cpu.cycleCount
+    
+    try operate(cpu: cpu, assemblingResults: results)
+}
+
+enum OperationMode {
+    case nextStep
+    case endInstruction
+    case run
+    case stop
+}
+
+func getCPUString(cpu: CPU, assemblingResults: AssemblingResults) -> String {
+    let literalValueType = AssemblingResults.LiteralAddressValue()
+    
+    let accuString = literalValueType.transform(value: cpu.accumulator)
+    
+    var result = "CPU:\n"
+    result += "State: \(cpu.state)\n\n"
+    result += "Program Counter:  \(toLongHexString(cpu.programCounter))\n"
+    result += "Stackpointer:     \(toLongHexString(cpu.stackpointer))\n"
+    result += "Accumulator:      \(accuString)\n"
+    result += "Execution Cycles: \(executionCycles)\n\n"
+    result += "Flags: "
+    
+    result += "n" + (cpu.nFlag ? "*" : " ") + "    "
+    result += "z" + (cpu.zFlag ? "*" : " ") + "    "
+    result += "v" + (cpu.vFlag ? "*" : " ") + "\n\n"
+    
+    result += "Operator\t\(getOperatorString(cpu: cpu))\n"
+    result += "Operand \t\(getOperandString(cpu: cpu, assemblingResult: assemblingResults))"
+    
+    result += "\n"
     
     return result
 }
 
-func printAll(cpu: CPU, mem: Memory, parseResults: ParseResults, continueRunningAllowed: Bool) -> Bool {
-    if continueRunningAllowed {
-        print("Execution successfull.")
+func getOperatorString(cpu: CPU) -> String {
+    if cpu.operator != nil && cpu.operandType != nil {
+        return cpu.operatorString + cpu.operandType!.representationAddition
     }
-    print("\n\(getCPUString(cpu: cpu))\n\(getVars(parseResult: parseResults, mem: mem))\n\(getMemoryRepresentation(mem: mem, results: parseResults))")
     
-    while true {
-        print("What should be done next?\n'P' to print the memory without transformation or additional information.")
+    return toLongHexString(cpu.opcode)
+}
+
+func getOperandString(cpu: CPU, assemblingResult result: AssemblingResults) -> String {
+    let operandAddress = cpu.operatorProgramCounter &+ 1
+    
+    let valueType = result.memoryValues[operandAddress] ?? AssemblingResults.AddressAddressValue()
+    
+    return valueType.transform(value: cpu.operand)
+}
+
+func getMarkerOfAType(assemblingResults results: AssemblingResults, mem: Memory, markerType: AssemblingResults.Marker.`Type`, title: String) -> String {
+    var result = "\(title)\n"
+    let markers = results.markers.filter() { marker in
+        marker.type == markerType
+    }
+    
+    if markers.count < 1 {
+        return ""
+    }
+    
+    for marker in markers {
         
-        if continueRunningAllowed {
-            print("'C' to continue running the CPU after the HOLD-instruction")
+        result += "\"\(marker.name)\" at 0x\(toHexString(marker.address)): "
+        
+        let valueType = results.memoryValues[marker.address] ?? AssemblingResults.AddressAddressValue()
+        
+        let representation = valueType.transform(value: mem.read(address: marker.address))
+        let numericRespresentation = valueType.transformOnlyNumber(value: mem.read(address: marker.address))
+        
+        result += representation
+        
+        if representation != numericRespresentation {
+            result += " (\(numericRespresentation))"
         }
-        print("Everything else will end the program.")
         
-        let answer = readLine()
+        result += "\n"
+    }
+    
+    result += "\n"
+    
+    return result
+}
+
+func getMarkers(assemblingResults results: AssemblingResults, mem: Memory) -> String {
+    
+    var result = getMarkerOfAType(assemblingResults: results, mem: mem, markerType: .variable, title: "Variables:")
+    
+    result += getMarkerOfAType(assemblingResults: results, mem: mem, markerType: .jumpMarker, title: "Jump-Markers:")
+    
+    result += getMarkerOfAType(assemblingResults: results, mem: mem, markerType: .undefined, title: "Undefined Markers")
+    
+    return result
+}
+
+func getOperationMode(cpu: CPU) -> OperationMode {
+    while true {
+        print("What should be done next?")
+        print("")
         
-        if answer == "P" {
-            print("\n\(mem)")
-        } else if answer == "C" && continueRunningAllowed {
-            return true
-        } else {
-            return false
+        print("'P' to print the memory without transformation or additional information")
+        print("'H' to print the memory without transformation or additional information in hexadecimal format")
+        print("'R' to run the CPU until the next HOLD-instruction")
+        print("'S' to operate the next step")
+        print("'E' to end the program")
+        print("")
+        
+        print("Everything else will end the current instruction.")
+        
+        let answer = readLine()?.lowercased()
+        
+        switch answer {
+            case "p":
+                printMemory(memory: cpu.memory)
+            case "h":
+                printHexMemory(memory: cpu.memory)
+            case "r":
+                return .run
+            case "s":
+                return .nextStep
+            case "e":
+                return .stop
+            default:
+                return .endInstruction
         }
     }
 }
 
-func parseCLIArgs(args: inout [String]) -> UInt16 {
-    for i in 0 ... args.count {
+func printMemory(memory: Memory) {
+    print("\n\(memory.getString())\n")
+}
+
+func printHexMemory(memory: Memory) {
+    print("\n\(memory.getHexString())\n")
+}
+
+func printAll(cpu: CPU, mem: Memory, assemblingResults: AssemblingResults) {
+    let cpuString = getCPUString(cpu: cpu, assemblingResults: assemblingResults)
+    let markers = getMarkers(assemblingResults: assemblingResults, mem: mem)
+    let memory = getMemoryRepresentation(cpu: cpu, mem: mem, results: assemblingResults)
+    
+    print("\(cpuString)\n\(markers)\n\(memory)\n")
+}
+
+func parseCLIArgs(args: inout [String]) -> ArgsResults {
+    var result = ArgsResults()
+    
+    var i = 0
+    
+    while i < args.count {
         let val = args[i]
-        if i == args.count - 1 {
-            return 0
-        }
         if val.lowercased() == "-s" {
-            if args[i+1].isNumberWithoutNegative {
-                let nrI = Int(args[i+1])!
-                if nrI > "FFFF".uHex {
+            if i + 1 >= args.count {
+                print("The starting point coudn't be read, there was no other parameter after '-s'\n\n")
+                break
+            }
+            
+            let param = args[i+1]
+            
+            let decimal = !param.contains() { char in !char.isNumber }
+            var hexadecimal: Bool {
+                let prefix = param.starts(with: "0x")
+                
+                let number = param.dropFirst().dropFirst()
+                
+                let numberAllowed = !number.contains() { char in !char.isHexDigit }
+                
+                return prefix && numberAllowed
+            }
+            
+            if decimal {
+                let nrI = Int(param)!
+                if nrI > 0xFFFF{
                     continue
                 }
                 let nr = UInt16(nrI)
                 args.remove(at: i)
                 args.remove(at: i)
-                return nr
-            } else if args[i+1].isHexadecimalWithoutNegative {
+                
+                i -= 2
+                
+                result.startingPoint = nr
+            } else if hexadecimal {
                 if args[i+1].count > 6 {
                     continue
                 }
-                let nr = args[i+1].uHex
+                let nr = try? hexFromString(param)
+                
                 args.remove(at: i)
                 args.remove(at: i)
-                return nr
+                result.startingPoint = nr
+                
+                i -= 2
+            } else {
+                print("The starting point coudn't be read, the parameter '\(param)' directely after '-s'\ncouldn't be converted into a number!\n\n")
             }
+        } else if val.lowercased() == "-r" {
+            result.instantRun = true
         }
+        
+        i += 1
     }
-    return 0
+    return result
+}
+
+struct ArgsResults {
+    var startingPoint: UInt16?
+    var instantRun = false
 }
